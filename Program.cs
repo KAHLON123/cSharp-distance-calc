bf7963bc813c62ff9f1b3ff4a7a04b08a7b866da
KAHLON123 <h.kahlon5@share.epsb.ca>
2023-01-16 12:55:08 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//welcome
+#nullable disable
+Console.Clear();
+Console.WriteLine("\nWelcome to the distance calculator");
+
+//inp
+Console.Write("enter x1 value: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("enter x2 value: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("enter y1 value: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("enter y2 value: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+//proc
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+//out
+Console.WriteLine($"The distance between these points is {distance}");
